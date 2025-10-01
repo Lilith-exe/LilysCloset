06ae77b72b9da2f63b40a2fc0223218c7ba8e0e6
Lilith.exe <lilith14.exe@gmail.com>
2025-10-01 20:41:38 +0100

    Wrapped app in Electron

---
+++ frontend/public/electron.js
@@ -0,0 +1,27 @@
+// public/electron.js
+const { app, BrowserWindow } = require("electron");
+const { autoUpdater } = require("electron-updater");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    title: "Lily's Closet",
+    icon: path.join(__dirname, "icon.ico"),
+    webPreferences: { nodeIntegration: false, contextIsolation: true },
+  });
+
+  const isDev = !app.isPackaged;
+  if (isDev) {
+    win.loadURL("http://localhost:3000");
+  } else {
+    win.loadFile(path.join(__dirname, "../build/index.html"));
+  }
+
+  if (!isDev) {
+    setTimeout(() => autoUpdater.checkForUpdatesAndNotify(), 3000);
+  }
+}
+
+app.whenReady().then(createWindow);
